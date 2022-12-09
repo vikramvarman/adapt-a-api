6b541afe863a1dba663cb5ad529821af244b8c15
Vikram <vikram@shortcastle.com>
2022-12-09 20:05:28 +0530

    swagger doc

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,10 @@ import express, { Express, Request, Response } from "express";
 import * as bodyParser from "body-parser";
 import routes from "./routes";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+
+const YAML = require("yamljs");
+const apiDocument = YAML.load("src/api.yaml");
 
 const app: Express = express();
 
@@ -9,6 +13,8 @@ app.use(cors());
 
 app.use(bodyParser.json({ limit: "1mb" }));
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(apiDocument));
 
 app.get("/ping", (req, res) => {
   return res.send("pong");
